3ae5cec3795362725ce7ec952285972d60e69d2f
PUIPUI <puipui0829@gmail.com>
2017-02-22 16:41:57 +0900

    add prefab

---
+++ Assets/Script/Cube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    private Transform tr;
+    public float rotSpeed = 100.0f;
+    // Use this for initialization
+    void Start () {
+        tr = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime);
+    }
+}
