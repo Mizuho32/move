b36102415f7d660e9877602db00ba02b731b71db
PUIPUI <puipui0829@gmail.com>
2017-02-22 15:29:47 +0900

    move with keybord

--- Assets/Script/Init.cs
+++ Assets/Script/Init.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Init : MonoBehaviour {
+public class Init : MonoBehaviour
+{
 
     // Use this for initialization
     private Vector3 initRot;
-	void Start () {
+    void Start()
+    {
         //var size = 20;
         //for(var x = 0; x < size; x++) 
         //    for(var y = 0; y < size; y++)
@@ -23,13 +25,14 @@ public class Init : MonoBehaviour {
     private const float sensX = 6.0f, sensY = 6.0f;
     private float speed = 0.0f;
     private Vector3? clicked = null;
-    private Vector3 centor = new Vector3(Screen.width/2, Screen.height/2, 0);
+    private Vector3 centor = new Vector3(Screen.width / 2, Screen.height / 2, 0);
     private Quaternion q;
-    void Update () {
-        
+    void Update()
+    {
 
 
-	    if (Input.GetMouseButton(0))
+
+        if (Input.GetMouseButton(0))
         {
             if (!clicked.HasValue)
             {
@@ -41,22 +44,25 @@ public class Init : MonoBehaviour {
             Vector3 delta = Input.mousePosition - clicked.Value;
             //transform.localEulerAngles = initRot + new Vector3( 0, 0, -delta.x/Screen.width*180f);
             //transform.localEulerAngles += new Vector3( -delta.y/Screen.height*100f, 0, 0);
-            transform.rotation = q * Quaternion.AngleAxis(-delta.x/Screen.width*180f, new Vector3(0,0,1)) * Quaternion.AngleAxis(-delta.y / Screen.height * 100f, new Vector3(1, 0, 0));
-            
+            transform.rotation = q * Quaternion.AngleAxis(-delta.x / Screen.width * 180f, new Vector3(0, 0, 1)) * Quaternion.AngleAxis(-delta.y / Screen.height * 100f, new Vector3(1, 0, 0));
+
 
             //transform.Rotate( (delta.y > 0f ? -1f : 1f)*Mathf.Pow(delta.y/Screen.height, 2)*sensY, 0, (delta.x > 0f ? -1f : 1f)*Mathf.Pow(delta.x/Screen.width,2)*sensX);
             //transform.Rotate( -delta.y/Screen.height*2*sensY, 0, -delta.x/Screen.width*2*sensX);
 
-        }else if (Input.GetMouseButton(1))
+        }
+        else if (Input.GetMouseButton(1))
         {
             transform.position += transform.TransformDirection(Vector3.up) * (-speed);
-        }else if (Input.GetMouseButton(2))
+        }
+        else if (Input.GetMouseButton(2))
         {
             speed = 0f;
-        }else
+        }
+        else
         {
             Vector3 delta = Input.mousePosition - centor;
-            transform.Rotate( 0, 0, -delta.x/Screen.width*sensX);
+            transform.Rotate(0, 0, -delta.x / Screen.width * sensX);
             transform.Rotate(-delta.y / Screen.height * sensY, 0, 0);
             //transform.rotation *= Quaternion.AngleAxis(-delta.x / Screen.width * sensX, new Vector3(0, 0, 1)) *
             //    Quaternion.AngleAxis(-delta.y / Screen.height * sensY, new Vector3(1,0,0));
@@ -77,5 +83,5 @@ public class Init : MonoBehaviour {
         transform.position += transform.TransformDirection(Vector3.up) * speed;
         //float deltax = Input.GetAxis("Mouse X");
         //Debug.logger.Log(deltax);
-	}
+    }
 }
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float h;
+    private float v;
+    private float rotX;
+    private float rotY;
+    private float speed = 0.0f;
+    private Transform tr;
+    public float moveSpeed = 30.0f;
+    public float rotSpeed = 100.0f;
+    //awake
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        rotX = Input.GetAxis("Mouse X");
+        rotY = Input.GetAxis("Mouse Y");
+
+        var wheel = -Input.GetAxis("Mouse ScrollWheel");
+        if (wheel > 0 && speed > -1) speed -= 0.2f;
+        if (wheel < 0 && speed < 1) speed += 0.2f;
+        if (Input.GetMouseButton(2)) speed = 0.0f;
+
+        Debug.Log("H=" + h.ToString());
+        Debug.Log("V=" + v.ToString());
+
+        tr.Translate(Vector3.forward * moveSpeed * speed * Time.deltaTime);
+        tr.Translate(Vector3.right * moveSpeed * h * Time.deltaTime);
+        tr.Translate(Vector3.up * moveSpeed * v * Time.deltaTime);
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * rotX);
+        tr.Rotate(Vector3.left * rotSpeed * Time.deltaTime * rotY);
+    }
+}
