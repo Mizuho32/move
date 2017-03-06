d604a84bf5f48c95cf3f1c335b2dd313883e174a
PUIPUI <puipui0829@gmail.com>
2017-03-06 15:28:13 +0900

    Delete Bullet if it go to far

--- Assets/Script/BulletCtrl.cs
+++ Assets/Script/BulletCtrl.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class BulletCtrl : MonoBehaviour
 {
     public float speed = 1000.0f;
+    private Transform tr;
     // Use this for initialization
     void Start()
     {
         GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Mathf.Pow(tr.position.x, 2) + Mathf.Pow(tr.position.y, 2) + Mathf.Pow(tr.position.z, 2) >= 1000*1000)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
